2ac38ff55a96673b74ea13d930b6707e87e28412
shimniok@gmail.com <shimniok@gmail.com>
2015-05-06 08:41:12 -0600

    Added feature to hide orders if # listed in hide.txt. Added 'list is empty' message.

--- orders.py
+++ orders.py
@@ -5,6 +5,8 @@ import locale
 import cgitb
 import datetime
 from Config import Config
+import sys
+import codecs
 
 # enable debugging
 cgitb.enable()
@@ -37,7 +39,16 @@ else:
     print("<th class=\"center\">Postage</th>")
     print("<th class=\"center\">Slip</th>")
 
+    with open('hide.txt', 'r') as f:
+        hide_list = f.read().splitlines()
+
+    count = 0
+
     for o in orders:
+
+        if str(o['number']) in hide_list:
+            continue
+
         paid = "PAID"
         for i in o['items']:
             if i['status'] == 'pending':
@@ -45,18 +56,24 @@ else:
                 break
 
         print("<tr>")
-        print("<td class=\"center\"><a href=\"https://www.tindie.com/orders/{0}/\">{0}</a></td>".format(o['number']))
+        print("<td class=\"center\"><a href=\"https://www.tindie.com/orders/{0}/\" target=\"_blank\">{0}</a></td>".format(o['number']))
         d = datetime.datetime.strptime(o["date"], "%Y-%m-%dT%H:%M:%S.%f")
         print("<td class=\"center\">{0}</td>".format(paid))
         print("<td class=\"center\">{0}</td>".format(d.strftime('%b %d, %Y %H:%M')))
         print("<td class=\"left\"><a href=\"mailto:{0}?subject=Tindie%20Order%20#{1}\">{0}</a>".format(o['email'], o['number']))
-        name = o['shipping_name']
+        name = o['shipping_name'].encode('utf-8')
         print("<td class=\"left\">{0}</td>".format(name[:20] + (name[20:] and '..')))
         print("<td class=\"center\"><a href=\"dazzle.py?order={0}\" target=\"_blank\">postage</a>".format(o['number']))
         print("<td class=\"center\"><a href=\"slip.py?order={0}\" target=\"_blank\">packing slip</a>".format(o['number']))
         print("</tr>")
 
+        count += 1
+
     print("</tbody></table>")
+
+    if count == 0:
+        print("List is empty.<br/>")
+
     print("<br/>")
     print("<a href='{0}&username={1}&api_key={2}'>Link to JSON data</a>".format(url, c.user, c.key))
     print("</body>")
